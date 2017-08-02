8b7380e1bbe2b6d590cac475e810ba4799f34e81
Staff <epicodus_staff@staff-2.local>
2017-08-02 09:24:28 -0700

    add products controller with new and create methods

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,23 @@
 class ProductsController < ApplicationController
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      flash[:notice] = "You successfully created a product!"
+      session[:product_id] = @product.id
+      redirect_to "/"
+    else
+      flash[:alert] = "There was a problem saving this product."
+      redirect_to '/product/new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:product).permit(:name, :cost, :description, :image)
+  end
 end
